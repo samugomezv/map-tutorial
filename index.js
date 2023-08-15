7801180e8b180ce13bbf8d2e8f58ca76b5b038cc
samugomezv <38594655+samugomezv@users.noreply.github.com>
2023-08-15 17:05:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = "pk.eyJ1Ijoic2FtdWdvbWV6diIsImEiOiJjbGw0N3VqbDYwMGl2M2ptbjB3a3djOTRiIn0.6Ctc1uc9EsgArIwolghArg";
+const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/samugomezv/cll5ky45700gy01qk0ad43mve", // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
